2cb8b08363415b9d92c0557139d5277edb97010d
ggcow <ggcow@mail.com>
2021-06-12 21:06:12 +0200

    added blue bird

--- ressources.py
+++ ressources.py
@@ -1,4 +1,4 @@
 from common import *
 
-bird = pygame.image.load(file_path('sprites/bird.png')).convert_alpha()
+bird = pygame.image.load(file_path('sprites/bird_blue.png')).convert_alpha()
 player = pygame.image.load(file_path('sprites/plane.png')).convert_alpha()
